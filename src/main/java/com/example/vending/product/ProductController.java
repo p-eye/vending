78242a69203c70d7c91844f6bee79f9e05c52378
p-eye <standingtherain15@gmail.com>
2021-06-29 19:31:55 +0900

    move index.html to /templates/ and test 'th:text'

--- src/main/java/com/example/vending/product/ProductController.java
+++ src/main/java/com/example/vending/product/ProductController.java
@@ -1,13 +1,15 @@
 package com.example.vending.product;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
 
-    @RequestMapping("/home")
-    public String home() {
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "this is coffee");
         return "index.html";
     }
 }

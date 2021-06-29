6b8c3d19c2cae8d91827ccd881a568029dd70aeb
p-eye <standingtherain15@gmail.com>
2021-06-29 14:19:31 +0900

    run with excluding DataSourceAutoConfiguratin (ProductController)

--- src/test/java/com/example/vending/product/ProductControllerTest.java
+++ src/test/java/com/example/vending/product/ProductControllerTest.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;

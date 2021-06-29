d1848e5b5d6d7c76d336132b891cbebbfd27aa4d
p-eye <standingtherain15@gmail.com>
2021-06-29 14:02:09 +0900

    test 'GET' method (ProductController)

---
+++ src/test/java/com/example/vending/product/ProductControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.vending.product;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHello() throws Exception {
+        String hello = "hello testing";
+
+        mvc.perform(get("/hello"))
+                .andExpect((status().isOk()))
+                .andDo(print());
+    }
+}

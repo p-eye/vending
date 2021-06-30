bb640aa7b7ff394d22c11a7051d4294b76119782
p-eye <standingtherain15@gmail.com>
2021-06-30 22:24:07 +0900

    form post test

---
+++ src/main/java/com/example/vending/controller/ArticleController.java
@@ -0,0 +1,21 @@
+package com.example.vending.controller;
+
+import com.example.vending.dto.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm articleForm) {
+        System.out.println(articleForm.toString());
+        return "";
+    }
+}

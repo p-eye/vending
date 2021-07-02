699f46cf0c273969f47f801f90e08fafe256b714
p-eye <standingtherain15@gmail.com>
2021-07-02 18:08:57 +0900

    divide checkColumn into isTable*s

--- src/main/java/com/example/vending/helper/ExcelHelper.java
+++ src/main/java/com/example/vending/helper/ExcelHelper.java
@@ -4,6 +4,7 @@ import com.example.vending.entity.Product;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -15,37 +16,87 @@ public class ExcelHelper extends CommonHelper {
             Sheet sheet = workbook.getSheet("product");
             if (sheet == null)
                 throw new IllegalAccessException("해당 시트가 존재하지 않습니다");
-            return checkColumns(sheet);
-
+            return sheetToProducts(sheet);
         } catch (Exception e) {
             System.out.println(e);
         }
         return null;
     }
 
-    private static List<Product> checkColumns(Sheet sheet) {
+    private static List<Product> sheetToProducts(Sheet sheet) {
         try {
-            Row headerRow = sheet.getRow(0);
-            if (headerRow == null)
-                throw new IllegalAccessException("해당 테이블이 존재하지 않습니다");
-            if (headerRow.getFirstCellNum() != 0 && headerRow.getLastCellNum() != 3)
-                throw new IllegalAccessException("테이블 column 개수가 올바르지 않습니다");
-            Cell header1 = headerRow.getCell(0);
-            Cell header2 = headerRow.getCell(1);
-            Cell header3 = headerRow.getCell(2);
-            if (header1 == null || header2 == null || header3 == null
-                || !"name".equals(header1.getStringCellValue())
-                || !"title".equals(header2.getStringCellValue())
-                || !"content".equals(header3.getStringCellValue()))
-                throw new IllegalAccessException("테이블 header 내용이 올바르지 않습니다");
+            if (!isTable(sheet) || !isTableHeader(sheet))
+                throw new IllegalAccessException("테이블이 올바르지 않습니다");
             return rowsToProducts(sheet);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
         return null;
     }
+
+    private static boolean isTable(Sheet sheet) {
+        int firstRowNum = sheet.getFirstRowNum();
+        int lastRowNum = sheet.getLastRowNum();
+        if (firstRowNum != 0)
+            return false;
+
+        for (int r = firstRowNum; r < lastRowNum; r++) {
+            Row row = sheet.getRow(r);
+            if (row == null)
+                return false;
+
+            int firstCellNum = row.getFirstCellNum();
+            int lastCellNum = row.getLastCellNum();
+            if (firstCellNum != 0 || lastCellNum != 3)
+                return false;
+
+            for (int c = firstCellNum; c < lastCellNum; c++) {
+                Cell cell = row.getCell(c);
+                if (cell == null)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isTableHeader(Sheet sheet) {
+
+        Row headerRow = sheet.getRow(0);
+        Cell header1 = headerRow.getCell(0);
+        Cell header2 = headerRow.getCell(1);
+        Cell header3 = headerRow.getCell(2);
+
+        if (header1 == null || header2 == null || header3 == null
+            || !"name".equals(header1.getStringCellValue())
+            || !"title".equals(header2.getStringCellValue())
+            || !"content".equals(header3.getStringCellValue()))
+            return false;
+        return true;
+    }
+
     private static List<Product> rowsToProducts(Sheet sheet) {
+
+        List<Product> products = new ArrayList<>();
+        String name, title, content;
+        int rowNum = 0;
+        Iterator<Row> rows = sheet.iterator();
+        while (rows.hasNext()) {
+            Row currentRow = rows.next();
+            System.out.println(currentRow.getRowNum());
+            /*
+            Row currentRow = rows.next();
+
+            Iterator<Cell> cells = currentRow.iterator();
+            while (cells.hasNext())
+            {
+                Cell currentCell = cells.next();
+                currentCell.get
+            }
+            Product product = new Product();
+            */
+
+        }
         return null;
     }
+
 }

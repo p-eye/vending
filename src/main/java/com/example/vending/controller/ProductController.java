f0db8d6462fb5edd76a223048093740de0e3d31a
p-eye <standingtherain15@gmail.com>
2021-07-02 15:05:37 +0900

    create Sheet from excelfile 'product' sheet

--- src/main/java/com/example/vending/controller/ProductController.java
+++ src/main/java/com/example/vending/controller/ProductController.java
@@ -5,9 +5,14 @@ import com.example.vending.entity.Product;
 import com.example.vending.helper.ExcelHelper;
 import com.example.vending.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -39,7 +44,16 @@ public class ProductController {
 
     @PostMapping("/product/create/file")
     public String saveFile(MultipartFile file) {
-       System.out.println(ExcelHelper.isExcelFormat(file));
+        if (ExcelHelper.isExcelFormat(file)) {
+            try (Workbook workbook = WorkbookFactory.create(file.getInputStream())) {
+                Sheet sheet = workbook.getSheet("product"); // 여기서 null 처리
+                int rowCnt = sheet.getPhysicalNumberOfRows(); // 공백 row 구분없이 전체 row 가져와서 쓰면 안됨
+                System.out.println(rowCnt);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
         return "redirect:/product/new";
     }
 

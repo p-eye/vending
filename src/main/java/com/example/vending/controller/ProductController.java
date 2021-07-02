10c128451effa4692a57f5a0da8d552108c86280
p-eye <standingtherain15@gmail.com>
2021-07-02 15:32:11 +0900

    create ExcelHelper.openFile function

--- src/main/java/com/example/vending/controller/ProductController.java
+++ src/main/java/com/example/vending/controller/ProductController.java
@@ -45,13 +45,7 @@ public class ProductController {
     @PostMapping("/product/create/file")
     public String saveFile(MultipartFile file) {
         if (ExcelHelper.isExcelFormat(file)) {
-            try (Workbook workbook = WorkbookFactory.create(file.getInputStream())) {
-                Sheet sheet = workbook.getSheet("product"); // 여기서 null 처리
-                int rowCnt = sheet.getPhysicalNumberOfRows(); // 공백 row 구분없이 전체 row 가져와서 쓰면 안됨
-                System.out.println(rowCnt);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+            ExcelHelper.openFile(file);
         }
 
         return "redirect:/product/new";
--- src/main/java/com/example/vending/helper/ExcelHelper.java
+++ src/main/java/com/example/vending/helper/ExcelHelper.java
@@ -1,5 +1,8 @@
 package com.example.vending.helper;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 public class ExcelHelper {
@@ -9,5 +12,15 @@ public class ExcelHelper {
         if (EXCEL.equals(file.getContentType()))
             return true;
         return false;
+    }
+
+    public static void openFile(MultipartFile file) {
+        try (Workbook workbook = WorkbookFactory.create(file.getInputStream())) {
+            Sheet sheet = workbook.getSheet("product"); // 여기서 null 처리
+            int rowCnt = sheet.getPhysicalNumberOfRows(); // 공백 row 구분없이 전체 row 가져와서 쓰면 안됨
+            System.out.println(rowCnt);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }

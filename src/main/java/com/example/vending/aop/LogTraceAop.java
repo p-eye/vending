d4b56f94e2890f0157156221eb43f01589bf5bf8
p-eye <standingtherain15@gmail.com>
2021-07-20 13:44:59 +0900

    logging with aop

---
+++ src/main/java/com/example/vending/aop/LogTraceAop.java
@@ -0,0 +1,19 @@
+package com.example.vending.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LogTraceAop {
+
+    @Around("execution(* com.example.vending..*(..))")
+    public Object printLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.debug("START: " + joinPoint.toString());
+        return joinPoint.proceed();
+    }
+}
